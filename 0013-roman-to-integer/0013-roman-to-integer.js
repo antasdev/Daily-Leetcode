8d931ea008093e4b1338e6d2dcf4608168c5c274
ANTAS C ANTONY <antonyantas03@gmail.com>
2026-06-09 11:56:14 +0530

    Time: 4 ms (73.64%), Space: 60.8 MB (71.94%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -13,8 +13,11 @@ var romanToInt = function(s) {
         M: 1000
     };
 let total=0;
+
 for(let i=0;i<s.length;i++){
-if(map[s[i]]<map[s[i+1]]){
+    let curr=map[s[i]];
+let aft=map[s[i+1]];
+if(curr<aft){
 total-=map[s[i]];
 }else{
 total+=map[s[i]]
