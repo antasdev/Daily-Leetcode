0801b0665690d3e7d69ffab69a0291dd661a6f88
ANTAS C ANTONY <antonyantas03@gmail.com>
2026-06-10 20:33:35 +0530

    Time: 0 ms (100%), Space: 55.4 MB (59.39%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+     const stack = [];
+
+    const map = {
+        ")": "(",
+        "}": "{",
+        "]": "["
+    };
+
+    for (let char of s){
+
+        if (char==="(" || char ==="{" || char==="["){
+            stack.push(char);
+        } else {
+            const top=stack.pop();
+
+            if (top!==map[char]) {
+                return false;
+            }
+        }
+    }
+    return stack.length=== 0;
+};
