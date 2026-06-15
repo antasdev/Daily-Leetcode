f8c8077601c1353041c9086b2a481d5d681e9c56
ANTAS C ANTONY <antonyantas03@gmail.com>
2026-06-15 09:22:02 +0530

    Time: 8 ms (37.15%), Space: 60 MB (15.15%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    const set=new Set();
+    for(let num of nums){
+        if(set.has(num)){
+            set.delete(num);
+        }else{
+            set.add(num)
+        }
+    }
+    return [...set][0]
+};
