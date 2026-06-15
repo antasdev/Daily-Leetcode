73cf97db1a43491b61cca16cad1cbc9b6051b17e
ANTAS C ANTONY <antonyantas03@gmail.com>
2026-06-15 09:29:43 +0530

    Time: 5 ms (44.36%), Space: 58.6 MB (24.11%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const count={}
+
+    for(let num of nums){
+        count[num]=(count[num]||0)+1;
+    if(count[num]>nums.length/2)
+    return num
+    }
+};
