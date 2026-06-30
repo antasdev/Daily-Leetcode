5f45976adc5ad4202f37e611737c23efc79c44f7
ANTAS C ANTONY <antonyantas03@gmail.com>
2026-06-30 16:40:14 +0530

    Time: 0 ms (100%), Space: 56 MB (36%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function(x, y, z) {
+    let d1=Math.abs(x-z);
+    let d2=Math.abs(y-z);
+    if(d1<d2)return 1
+    if(d1>d2)return 2
+    return 0;
+};
