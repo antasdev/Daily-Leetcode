c1d3a1ff9144e722f2587dd2f868a36cb8aba22b
ANTAS C ANTONY <antonyantas03@gmail.com>
2026-07-02 08:08:15 +0530

    Time: 71 ms (67.76%), Space: 64.7 MB (33.88%) - LeetHub

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+var countConsistentStrings = function(allowed, words) {
+    let set=new Set(allowed);
+    let count=0;
+
+    for (let word of words){
+        let valid=true;
+
+        for (let char of word){
+            if (!set.has(char)){
+                valid=false;
+                break;
+            }
+        }
+
+        if (valid){
+            count++;
+        }
+    }
+    return count;
+};
