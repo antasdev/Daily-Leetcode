ec0ac6794e9df67486b6066aed7603490e2e71ff
ANTAS C ANTONY <antonyantas03@gmail.com>
2026-07-07 16:51:21 +0530

    Time: 0 ms (100%), Space: 60 MB (25.41%) - LeetHub

---
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function(items, ruleKey, ruleValue) {
+    let index;
+    if(ruleKey==="type"){
+        index = 0;
+    } else if(ruleKey === "color"){
+        index=1;
+    }else{
+        index=2;
+    }
+    let count=0;
+    for (let item of items) {
+        if (item[index]===ruleValue) {
+            count++;
+        }
+    }
+    return count;
+};
