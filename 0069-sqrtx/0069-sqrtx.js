f25e384f671a570e716cab9053ebbfa0332b2c7c
ANTAS C ANTONY <antonyantas03@gmail.com>
2026-07-20 13:16:48 +0530

    Time: 0 ms (100%), Space: 57 MB (23.73%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,22 +3,25 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    let left=0
-    let right=x;
-    let ans=0
-    while(left<=right){
-        let mid=Math.floor((left+right)/2)
-        let square=mid*mid;
-        if(square===x){
-           return mid;
-        }
-        if(square<x){
-            ans=mid
-         left=mid+1
-        }
-        if(square>x){
-            right=mid-1
-        }
-    }
-    return ans
+    // let left=0
+    // let right=x;
+    // let ans=0
+    // while(left<=right){
+    //     let mid=Math.floor((left+right)/2)
+    //     let square=mid*mid;
+    //     if(square===x){
+    //        return mid;
+    //     }
+    //     if(square<x){
+    //         ans=mid
+    //      left=mid+1
+    //     }
+    //     if(square>x){
+    //         right=mid-1
+    //     }
+    // }
+    // return ans
+let ans= x**0.5
+
+    return Math.floor(ans,1)
 };
