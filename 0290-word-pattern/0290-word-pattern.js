b8462520de24ffd3346339fcaf7b1065f9ca9e1b
ANTAS C ANTONY <antonyantas03@gmail.com>
2026-07-21 10:05:14 +0530

    Time: 0 ms (100%), Space: 53.8 MB (45.07%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.js
+++ 0290-word-pattern/0290-word-pattern.js
@@ -5,19 +5,16 @@
  */
 var wordPattern = function(pattern, s) {
 const words = s.split(" ");
-
-    if (pattern.length !== words.length) {
+    if (pattern.length !== words.length){
         return false;
     }
-
     const charToWord = new Map();
     const wordToChar = new Map();
-
-    for (let i = 0; i < pattern.length; i++) {
+    for (let i = 0; i < pattern.length; i++){
         const ch = pattern[i];
         const word = words[i];
-        if (charToWord.has(ch)) {
-            if (charToWord.get(ch) != word) {
+        if (charToWord.has(ch)){
+            if (charToWord.get(ch) != word){
                 return false;
             }
         } else {
